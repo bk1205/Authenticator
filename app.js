ae007ea02088c4527703e8cf530ec1839a50b5d7
Your Name <2000bhaveshkumar@gmail.com>
2022-03-25 17:23:42 +0530

    deploy

--- app.js
+++ app.js
@@ -1,39 +1,47 @@
 //Entry Point
-const express = require('express');
-const session = require('express-session');
-const passport = require('passport');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
+const express = require("express");
+const session = require("express-session");
+const http = require("http");
+const passport = require("passport");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
 
-const authRoutes = require('./routes/auth_routes');
-const passportSetup = require('./config/passport_setup');
-const keys = require('./config/keys');
+const authRoutes = require("./routes/auth_routes");
+const passportSetup = require("./config/passport_setup");
+const keys = require("./config/keys");
 
 const app = express();
 
+const server = http.createServer(app);
 
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:3000",
     credentials: true,
-}));
+  })
+);
 
-mongoose.connect(keys.database.mongodbURI, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
-    console.log('connected to mongodb');
-});
+mongoose.connect(
+  keys.database.mongodbURI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("connected to mongodb");
+  }
+);
 
-
-
-app.use(cookieSession({
+app.use(
+  cookieSession({
     keys: [keys.session.cookieKey],
-    maxAge: 24*60*60*1000
-}));
+    maxAge: 24 * 60 * 60 * 1000,
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/auth', authRoutes);
+app.use("/auth", authRoutes);
 
-app.listen(4000, () => {
-    console.log('Server is listening on port 4000');
+server.listen(4000, () => {
+  console.log("Server is listening on port 4000");
 });
