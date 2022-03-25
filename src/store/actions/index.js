9ee0b537c7ca993e1904962ec033820f1b4e5c9c
Your Name <2000bhaveshkumar@gmail.com>
2022-03-25 22:25:13 +0530

    deploy

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { FETCH_USER, GET_STREAM } from "./types";
 
 export const fetchUser = () => async (dispatch) => {
-  const res = await axios.get("/auth/getUser");
+  const res = await axios.get("/auth/getUser", { withCredentials: true });
   console.log("reducer", res);
   dispatch({
     type: FETCH_USER,
