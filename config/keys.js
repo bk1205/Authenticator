7a0c875a14c43953867914954bd323ec98b3abd5
Your Name <2000bhaveshkumar@gmail.com>
2022-03-25 21:45:06 +0530

    deploy

--- config/keys.js
+++ config/keys.js
@@ -1,14 +1,15 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-    google: {
-        clientID : process.env.CLIENT_ID,
-        clientSecret : process.env.CLIENT_SECRET,
-    },
-    database: {
-        mongodbURI : "mongodb://localhost:27017/ChatRoom"
-    },
-    session: {
-        cookieKey: "bkumarIamDoyouknowme"
-    }
-}
+  google: {
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+  },
+  database: {
+    mongodbURI:
+      "mongodb+srv://bk1205:Kumar^2000@cluster0.a6h6f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  },
+  session: {
+    cookieKey: "bkumarIamDoyouknowme",
+  },
+};
