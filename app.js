f70799bd919a009cb028da1d9c65e96f7262cae1
Your Name <2000bhaveshkumar@gmail.com>
2022-03-26 21:30:43 +0530

    deploy

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require("express");
 const session = require("express-session");
 const http = require("http");
 const passport = require("passport");
-const cors = require("cors");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 
@@ -11,23 +10,27 @@ const authRoutes = require("./routes/auth_routes");
 const passportSetup = require("./config/passport_setup");
 const keys = require("./config/keys");
 
-const app = express();
+const cors = require("cors")({
+  origin: "https://oauthenticator.herokuapp.com",
+});
 
-const server = http.createServer(app);
-var whitelist = [
-  "https://oauthenticator.herokuapp.com",
-  "https://oauthenticatorbackend.herokuapp.com",
-];
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
-app.use(corsOptions);
+const app = express();
+app.use(cors);
+
+// const server = http.createServer(app);
+// var whitelist = [
+
+//   "https://oauthenticatorbackend.herokuapp.com",
+// ];
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+// };
 
 mongoose.connect(
   keys.database.mongodbURI,
@@ -56,6 +59,6 @@ app.use("/auth", authRoutes);
 
 port = process.env.PORT || 4000;
 
-server.listen(port, () => {
+app.listen(port, () => {
   console.log("Server is listening on port 4000");
 });
